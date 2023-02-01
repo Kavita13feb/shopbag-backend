4fb2f30b9d9d26e1a5247353163d53a9d14c29ae
Kavita mali <kavita13feb@gmail.com>
2023-02-01 12:18:07 +0530

    api

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
+const port =process.env.PORT ||8080
 
 server.use(middlewares)
 server.use(router)
-server.listen(3000, () => {
+server.listen(port, () => {
   console.log('JSON Server is running')
 })
